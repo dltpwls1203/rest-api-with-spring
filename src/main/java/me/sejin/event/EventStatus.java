6acbfc5f599d66a76daac829fb69ec9c58469357
LeeSejin <dltpwls1203@gmail.com>
2023-09-13 08:31:29 +0900

    first commit

---
+++ src/main/java/me/sejin/event/EventStatus.java
@@ -0,0 +1,6 @@
+package me.sejin.event;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/me/sejin/event/EventTest.java
@@ -0,0 +1,33 @@
+package me.sejin.event;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        // Given
+        String descrption = "Spring";
+        String name = "Event";
+
+        // When
+        Event event = new Event();
+        event.setName("Event");
+        event.setDescription("Spring");
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(descrption);
+    }
+}
